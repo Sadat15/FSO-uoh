3b9f3cecd88e95f1865478607dabe1584f61abf1
Sadat15 <sadatmanjur15@gmail.com>
2023-11-22 14:32:05 +0000

    Exercise 4.10 completed

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -77,6 +77,24 @@ test("verifies unique identifier property is named id", async () => {
   expect(response.body[0].id).toBeDefined();
 }, 100000);
 
+test("verifies that a post request successfully creates a new blog post", async () => {
+  let blogs = await api.get("/api/blogs");
+  const originalBlogsLength = blogs.body.length;
+
+  const newBlog = {
+    title: "Every Software Developer should write a blog",
+    author: "Elchin Nasirov",
+    url: "https://dev.to/nasirovelchin/every-software-developer-should-write-a-blog-4622",
+    likes: 147,
+  };
+
+  await api.post("/api/blogs", newBlog);
+
+  blogs = await api.get("/api/blogs");
+
+  expect(blogs.body).toHaveLength(originalBlogsLength + 1);
+}, 100000);
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
