046599ab16d81a59d471dc6c62301c92cf290265
Sadat15 <sadatmanjur15@gmail.com>
2023-12-11 14:15:09 +0000

    Exercise 4.15 completed

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 mongoose.connect(config.MONGODB_URI).then(console.log("Connected to MongoDB"));
 
@@ -13,5 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -3,6 +3,8 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const initialBlogs = [
   {
@@ -52,113 +54,155 @@ beforeEach(async () => {
   }
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("blog testing", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("the length of blog objects is correct", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body).toHaveLength(initialBlogs.length);
+  }, 100000);
+
+  test("the first blog is called React patterns", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body[0].title).toEqual("React patterns");
+  }, 100000);
+
+  test("verifies unique identifier property is named id", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body[0].id).toBeDefined();
+  }, 100000);
+
+  test("verifies that a post request successfully creates a new blog post", async () => {
+    let blogs = await api.get("/api/blogs");
+    const originalBlogsLength = blogs.body.length;
+
+    const newBlog = {
+      title: "Every Software Developer should write a blog",
+      author: "Elchin Nasirov",
+      url: "https://dev.to/nasirovelchin/every-software-developer-should-write-a-blog-4622",
+      likes: 147,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(201);
+
+    blogs = await api.get("/api/blogs");
+
+    expect(blogs.body).toHaveLength(originalBlogsLength + 1);
+  }, 100000);
+
+  test("verifies that if likes property is missing, will default the value to 0", async () => {
+    const newBlog = {
+      title: "Back-End & Web Development Trends For 2024",
+      author: "Kostya Stepanov",
+      url: "https://medium.com/ux-planet/back-end-web-development-trends-for-2024-04cc14bb43cb",
+    };
+    await api.post("/api/blogs").send(newBlog).expect(201);
+    let blogs = await api.get("/api/blogs");
+    blogs = blogs.body;
+
+    expect(blogs[blogs.length - 1].likes).toEqual(0);
+  }, 100000);
+
+  test("verifies that if title is missing, response is a 400 bad request", async () => {
+    const newBlog = {
+      title: "",
+      author: "Fresh Frontend Links",
+      url: "https://medium.com/@frontender-ua/frontend-weekly-digest-338-20-26-november-2023-0391292f6e58",
+      likes: 52,
+    };
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogs = await api.get("/api/blogs");
+
+    expect(blogs.body).toHaveLength(initialBlogs.length);
+  }, 100000);
+
+  test("verifies that delete request works", async () => {
+    const blogsAtStart = await api.get("/api/blogs");
+    const body = blogsAtStart.body;
+
+    const blogToDelete = body[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await api.get("/api/blogs");
+
+    expect(blogsAtEnd.body).toHaveLength(initialBlogs.length - 1);
+
+    const urls = blogsAtEnd.body.map((el) => el.url);
+
+    expect(urls).not.toContain(blogToDelete.url);
+  });
+
+  test("Verifies that the number of likes is updated correctly", async () => {
+    const blogsAtStart = await api.get("/api/blogs");
+    const body = blogsAtStart.body;
+
+    const blogToUpdate = body[0];
+
+    const updatedBlog = {
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 10,
+    };
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(204);
+
+    const blogsAtEnd = await api.get("/api/blogs");
+    const bodyAtEnd = blogsAtEnd.body;
+
+    expect(bodyAtEnd[0].likes).toEqual(10);
+  }, 100000);
 });
 
-test("the length of blog objects is correct", async () => {
-  const response = await api.get("/api/blogs");
+describe("user testing", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
 
-  expect(response.body).toHaveLength(initialBlogs.length);
-}, 100000);
+    const passwordHash = await bcrypt.hash("sekret", 10);
+    const user = new User({ username: "root", passwordHash });
 
-test("the first blog is called React patterns", async () => {
-  const response = await api.get("/api/blogs");
+    await user.save();
+  });
 
-  expect(response.body[0].title).toEqual("React patterns");
-}, 100000);
+  test("verifies that user database has one user", async () => {
+    const users = await api.get("/api/users");
 
-test("verifies unique identifier property is named id", async () => {
-  const response = await api.get("/api/blogs");
+    expect(users.body).toHaveLength(1);
+  });
 
-  expect(response.body[0].id).toBeDefined();
-}, 100000);
+  test("verifies that a post request successfully creates a new user", async () => {
+    const users = await api.get("/api/users");
 
-test("verifies that a post request successfully creates a new blog post", async () => {
-  let blogs = await api.get("/api/blogs");
-  const originalBlogsLength = blogs.body.length;
+    const newUser = {
+      username: "mluukkai",
+      name: "Matti Luukkainen",
+      password: "salainen",
+    };
 
-  const newBlog = {
-    title: "Every Software Developer should write a blog",
-    author: "Elchin Nasirov",
-    url: "https://dev.to/nasirovelchin/every-software-developer-should-write-a-blog-4622",
-    likes: 147,
-  };
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  await api.post("/api/blogs").send(newBlog).expect(201);
+    const updatedUsers = await api.get("/api/users");
 
-  blogs = await api.get("/api/blogs");
-
-  expect(blogs.body).toHaveLength(originalBlogsLength + 1);
-}, 100000);
-
-test("verifies that if likes property is missing, will default the value to 0", async () => {
-  const newBlog = {
-    title: "Back-End & Web Development Trends For 2024",
-    author: "Kostya Stepanov",
-    url: "https://medium.com/ux-planet/back-end-web-development-trends-for-2024-04cc14bb43cb",
-  };
-  await api.post("/api/blogs").send(newBlog).expect(201);
-  let blogs = await api.get("/api/blogs");
-  blogs = blogs.body;
-
-  expect(blogs[blogs.length - 1].likes).toEqual(0);
-}, 100000);
-
-test("verifies that if title is missing, response is a 400 bad request", async () => {
-  const newBlog = {
-    title: "",
-    author: "Fresh Frontend Links",
-    url: "https://medium.com/@frontender-ua/frontend-weekly-digest-338-20-26-november-2023-0391292f6e58",
-    likes: 52,
-  };
-  await api.post("/api/blogs").send(newBlog).expect(400);
-
-  const blogs = await api.get("/api/blogs");
-
-  expect(blogs.body).toHaveLength(initialBlogs.length);
-}, 100000);
-
-test("verifies that delete request works", async () => {
-  const blogsAtStart = await api.get("/api/blogs");
-  const body = blogsAtStart.body;
-
-  const blogToDelete = body[0];
-
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-  const blogsAtEnd = await api.get("/api/blogs");
-
-  expect(blogsAtEnd.body).toHaveLength(initialBlogs.length - 1);
-
-  const urls = blogsAtEnd.body.map((el) => el.url);
-
-  expect(urls).not.toContain(blogToDelete.url);
+    expect(updatedUsers.body).toHaveLength(users.body.length + 1);
+  });
 });
-
-test("Verifies that the number of likes is updated correctly", async () => {
-  const blogsAtStart = await api.get("/api/blogs");
-  const body = blogsAtStart.body;
-
-  const blogToUpdate = body[0];
-
-  const updatedBlog = {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 10,
-  };
-
-  await api.put(`/api/blogs/${blogToUpdate.id}`).send(updatedBlog).expect(204);
-
-  const blogsAtEnd = await api.get("/api/blogs");
-  const bodyAtEnd = blogsAtEnd.body;
-
-  expect(bodyAtEnd[0].likes).toEqual(10);
-}, 100000);
 
 afterAll(async () => {
   await mongoose.connection.close();
