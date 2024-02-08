62bf2f373d7a5b454014c31c08d0da098d5830ae
Sadat15 <sadatmanjur15@gmail.com>
2024-02-08 13:47:02 +0000

    Exercise 4.17 completed

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,17 +1,31 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
 
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response, next) => {
-  const blog = new Blog(request.body);
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body;
+  // const user = await User.findById(body.userId);
+  const users = await User.find({}).populate("blogs");
+  const user = users[0];
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  });
 
   try {
     const result = await blog.save();
+    user.blogs = user.blogs.concat(result._id);
+    await user.save();
     response.status(201).json(result);
   } catch (exception) {
     response.status(400).json({ error: exception.message });
@@ -31,6 +45,7 @@ blogsRouter.put("/:id", async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: body.user.id,
   };
 
   await Blog.findByIdAndUpdate(request.params.id, updatedBlog);
