25660d20d3741beca7f5707d4698ac60c10c3d8f
Sadat15 <sadatmanjur15@gmail.com>
2024-02-12 15:09:52 +0000

    Exercise 4.19 completed

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
 
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
@@ -13,9 +14,13 @@ mongoose.connect(config.MONGODB_URI).then(console.log("Connected to MongoDB"));
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
