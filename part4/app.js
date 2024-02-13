9a454eb416ec831e99eff71c86161dae6b12d002
Sadat15 <sadatmanjur15@gmail.com>
2024-02-13 18:18:23 +0000

    Exercise 4.20 completed

--- part4/app.js
+++ part4/app.js
@@ -15,6 +15,7 @@ mongoose.connect(config.MONGODB_URI).then(console.log("Connected to MongoDB"));
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
