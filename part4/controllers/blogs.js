4ac015a9bf9b337fd9a4298accd77a76cb098be3
Sadat15 <sadatmanjur15@gmail.com>
2024-02-14 18:24:05 +0000

    Exercise 4.22 completed

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,7 +1,6 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
-const User = require("../models/user");
-const jwt = require("jsonwebtoken");
+const middleware = require("../utils/middleware");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -9,14 +8,10 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", middleware.userExtractor, async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token invalid" });
-  }
-  const user = await User.findById(decodedToken.id);
+  const user = request.user;
 
   const blog = new Blog({
     title: body.title,
@@ -36,24 +31,25 @@ blogsRouter.post("/", async (request, response) => {
   }
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  const userid = decodedToken.id;
-  if (!userid) {
-    return response.status(401).json({ error: "token invalid" });
-  }
+blogsRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response) => {
+    const user = request.user;
+    const userid = user.id;
 
-  const blog = await Blog.findById(request.params.id);
+    const blog = await Blog.findById(request.params.id);
 
-  if (blog.user.toString() === userid.toString()) {
-    await Blog.findByIdAndDelete(request.params.id);
-    response.status(204).end();
-  } else {
-    return response.status(401).json({
-      error: "User not authorised to delete this blog",
-    });
+    if (blog.user.toString() === userid.toString()) {
+      await Blog.findByIdAndDelete(request.params.id);
+      response.status(204).end();
+    } else {
+      return response.status(401).json({
+        error: "User not authorised to delete this blog",
+      });
+    }
   }
-});
+);
 
 blogsRouter.put("/:id", async (request, response) => {
   const body = request.body;
